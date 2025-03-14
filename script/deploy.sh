3b1d3991c8a2b1fabbaebcde81918bb11298cd62
SanRob1325 <seanrobertblitzntonya@gmail.com>
2025-03-14 19:43:11 +0000

    Adding deployment

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,7 +5,7 @@ set -e
 if [ ! -d '/var/www/event_backend' ]; then
 
   sudo mkdir -p /var/www/event_backend
-  sudo chown ${EC2_USER}:${EC2_USER} /var/www/event_backend
+  sudo chown $USER:$USER /var/www/event_backend
   git clone https://github.com/DevOpsSecProject/event_backend.git /var/www/event_backend
 else
   cd /var/www/event_backend
@@ -18,12 +18,13 @@ bundle config set --local path 'vendor/bundle'
 bundle install
 
 # Run migrations
-SECRET_KEY_BASE=${SECRET_KEY_BASE} RAILS_ENV=production bundle exec rails db:migrate
+export SECRET_KEY_BASE=$"SECRET_KEY_BASE"
+RAILS_ENV=production bundle exec rails db:migrate
 # Restart Rails server (kill the current process running)
 pkill -f puma || true
 
 # Start server as background proccess
 cd /var/www/event_backend
-SECRET_KEY_BASE=${SECRET_KEY_BASE} RAILS_ENV=production bundle exec rails server -b 0.0.0.0 -p 3000 > /var/www/event_backend/server.log 2>&1 &
+RAILS_ENV=production bundle exec rails server -b 0.0.0.0 -p 3000 > /var/www/event_backend/server.log 2>&1 &
 
 echo "Deployment completed successfully"
