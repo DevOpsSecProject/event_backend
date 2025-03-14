88718c3f00229c2cf5b4eb160be3238660712182
SanRob1325 <seanrobertblitzntonya@gmail.com>
2025-03-14 17:11:19 +0000

    Added tests for configuration

--- spec/requests/favourites_spec.rb
+++ spec/requests/favourites_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "Favourites", type: :request do
   let(:user) { create(:user) }
   let(:event) { create(:event) }
-  let(:valid_attributes) {{ user_id:user.id, event_id: event.id}}
-  let(:invalid_attributes) {{ user_id:nil, event_id:nil}}
+  let(:valid_attributes) { { user_id: user.id, event_id: event.id } }
+  let(:invalid_attributes) { { user_id: nil, event_id: nil } }
 
   describe "GET /index" do
     it "renders a successful response" do
@@ -12,6 +12,5 @@ RSpec.describe "Favourites", type: :request do
       get favourites_url, as: :json
       expect(response).to be_successful
     end
-
   end
 end
