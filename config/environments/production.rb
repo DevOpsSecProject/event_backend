d291131f19da4b706082bc413dfaa2a92b31195e
SanRob1325 <seanrobertblitzntonya@gmail.com>
2025-03-24 11:23:22 +0000

    added smoother TLS/SSL certificat future implementation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+  #   /.*\.example\.com/ # Allow requests from subdomains like `server.js.example.com`
   # ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
