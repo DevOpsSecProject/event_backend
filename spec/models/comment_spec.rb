5155700126868ce787f4f892335da85c2ca0d329
SanRob1325 <seanrobertblitzntonya@gmail.com>
2025-03-25 20:31:03 +0000

    Added comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe 'associations' do
+    # Comment assigned to users is optional
     it { should belong_to(:user).optional }
+    # Every comment belongs to an event
     it { should belong_to(:event) }
   end
+  # verifies comment must have content and prevents empty comments
   describe 'validations' do
     it { should validate_presence_of(:content) }
   end
